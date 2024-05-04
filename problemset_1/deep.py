18eac888d56354718ec38bf11972c6a0a287d659
JANESHNANDA K S <137386817+janesh-kiddev@users.noreply.github.com>
2024-05-04 19:15:27 +0530

    Create deep.py

---
+++ problemset_1/deep.py
@@ -0,0 +1,7 @@
+def main():
+    ques=input("What Is The Answer To The Great Question Of Life?")
+    if ques=='42' or ques=='forty-two' or ques=='forty two':
+        print('Yes')
+    else:
+        print('No')
+main()
